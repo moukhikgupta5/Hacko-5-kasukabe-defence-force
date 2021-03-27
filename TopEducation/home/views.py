f0130a478a62896127ed2a2bcd4c79d9c8029670
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2021-03-27 14:01:15 +0530

    All errors fixed Upto Login

--- TopEducation/home/views.py
+++ TopEducation/home/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth import logout,authenticate,login
 
 def home(request):
+    user = request.user
+    username = user.username
+    context = {'username' : username}
     if request.user.is_anonymous:
         return redirect('/login')
     return HttpResponse('<h>Welcome to the Home page!</h1>')
@@ -11,6 +14,8 @@ def loginuser(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        print(username)
+        print(password)
         user = authenticate(username = username,password = password)
         if user is not None:
             login(request, user)
