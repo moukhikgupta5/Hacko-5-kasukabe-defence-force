d6f14b6b94878f5704ca91b491b18dcc3741b674
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2021-03-27 09:44:03 +0530

    Authnetication System

--- TopEducation/home/views.py
+++ TopEducation/home/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import logout,authenticate,login
+
 def home(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
     return render(request,'home.html')
-def login(request):
-    # if request.method == 'POST':
-    return HttpResponse('<h>Welcome to the login page!</h1>')
-def logout(request):
-    return HttpResponse('<h>Welcome to the logout page!</h1>')
+def loginuser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        username = request.POST.get('password')
+        user = authenticate(username = username,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/home')
+        else:
+            return HttpResponse('<h>Welcome to the login page!</h1>')
+    return HttpResponse('<h1>Welcome to the login page!</h1>')
+def logoutuser(request):
+    logout(request)
+    return redirect('/login')
