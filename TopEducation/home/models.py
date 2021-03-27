bd069cb9cca8543329ec467837660db09a546617
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2021-03-27 19:26:33 +0530

    Added Student Table

--- TopEducation/home/models.py
+++ TopEducation/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField (max_length=50)
+    rollno = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField( max_length=50)
+    branch = models.CharField( max_length=50) 
+    fName = models.CharField( max_length=50) 
+    city = models.CharField( max_length=50) 
+    dob = models.DateField()
