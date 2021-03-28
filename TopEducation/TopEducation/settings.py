6bbfe466935684ca60fc716cfcce14daafbcab1a
Moukhik Gupta <72201222+moukhikgupta5@users.noreply.github.com>
2021-03-28 09:38:58 +0530

    Email feature paused

--- TopEducation/TopEducation/settings.py
+++ TopEducation/TopEducation/settings.py
@@ -119,4 +119,4 @@ EMAIL_HOST_USER = 'moukhikgupta5@gmail.com'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'moukhikgupta5@gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'Mgarg592002'
+EMAIL_HOST_PASSWORD = ' '
